44821ec4bae073759de283963d490116b3d7e48f
ArturLeszczynski <artur.leszczynski.gdynia@gmail.com>
2022-10-06 23:35:33 +0200

    MySqlConnection upgrade

--- src/main/java/MySqlConnection.java
+++ src/main/java/MySqlConnection.java
@@ -4,13 +4,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MySqlConnection {
-    public static void main(String[] args) {
+
+    static Connection connection;
+    static Statement statement;
+
+    public MySqlConnection() {
         String url = "jdbc:mysql://localhost:3306/water-biling-system";
         String user = "root";
         String password = "root";
-        Connection connection;
-        Statement statement;
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
